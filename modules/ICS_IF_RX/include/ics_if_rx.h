b7a4b76b88b8cff4b9b0f511399ef0820d1eb771
Akira Nishiyama <mr081677@gmail.com>
2020-10-17 02:08:03 +0900

    implement receive char function

--- modules/ICS_IF_RX/include/ics_if_rx.h
+++ modules/ICS_IF_RX/include/ics_if_rx.h
@@ -17,17 +17,20 @@
 #include <ap_int.h>
 #include <hls_stream.h>
 
-typedef ap_uint<1> uint1_t; // 1-bit user defined type
+typedef ap_uint<1>  uint1_t; // 1-bit user defined type
+typedef ap_uint<5>  uint5_t; // 5-bit user defined type
 //typedef ap_uint<8> uint8_t; // 8-bit user defined type
+typedef ap_uint<9>  uint9_t; // 5-bit user defined type
+typedef ap_uint<10> uint10_t; // 5-bit user defined type
 
 void ics_if_rx(
 	hls::stream<uint1_t> &ics_sig_i,
-	hls::stream<uint8_t> &ics_char_o,
-	ap_uint<10> bit_period_i
+	hls::stream<uint9_t> &ics_char_o,
+	uint10_t bit_period_i
 );
 
 ap_uint<1> ics_rx_bit_judge(
 	hls::stream<uint1_t> &ics_sig_i,
-	ap_uint<10> bit_period_i
+	uint10_t bit_period_i
 );
 #endif
--- modules/ICS_IF_RX/test/src/tb_ics_if_rx.cpp
+++ modules/ICS_IF_RX/test/src/tb_ics_if_rx.cpp
@@ -15,136 +15,179 @@
 #include <stdint.h>
 #include <ap_int.h>
 #include <hls_stream.h>
+#include "gtest/gtest.h"
 #include "ics_if_rx.h"
 
 void input_stream(
-	hls::stream<uint1_t> &ics_sig,
-	unsigned char ch,
-	ap_int<10> bit_period
+    hls::stream<uint1_t> &ics_sig,
+    unsigned char ch,
+    uint10_t bit_period,
+	uint1_t parity_err_flag
 ){
-	ap_uint<10> tx_ch = (ch & 0x0ff) * 2 + 1;//add start bit and stop bit.
-	for(int i = 0; i < 10; ++i){
-		for(int j = 0; j < bit_period; ++j){
-			ics_sig.write_nb((tx_ch >> (10 - i - 1) & 0x01));
-		}
-	}
+    ap_uint<8> parity = ch;
+    parity ^= parity >> 4;
+    parity ^= parity >> 2;
+    parity ^= parity >> 1;
+    ap_uint<11> tx_ch = (ch & 0x0ff) * 4 + ((parity + parity_err_flag) & 0x01) * 2 + 1;//add start bit, parity and stop bit.
+    for(int i = 0; i < 10; ++i){
+        for(int j = 0; j < bit_period; ++j){
+            ics_sig.write_nb((tx_ch >> (10 - i) & 0x01));
+        }
+    }
 }
 
 void input_idle(
-	hls::stream<uint1_t> &ics_sig,
-	int count
+    hls::stream<uint1_t> &ics_sig,
+    int count
 ){
-	for(int i = 0; i < count; ++i){
-		ics_sig.write_nb(1);
-	}
+    for(int i = 0; i < count; ++i){
+        ics_sig.write_nb(1);
+    }
+}
+class IcsIfRxTest : public testing::Test {
+    protected:
+    ap_uint<10> bit_period = 87;
+    hls::stream<uint9_t> ics_char;
+    hls::stream<uint9_t> ics_char_exp;
+    hls::stream<uint1_t> ics_sig_i;
+    
+    IcsIfRxTest() {
+    }
+    
+    virtual ~IcsIfRxTest() {
+    }
+    
+    virtual void SetUp() {
+    }
+    
+    virtual void TearDown() {
+    }
+};
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
 
-int main(void){
-	ap_uint<10> bit_period = 87;
-	ap_uint<5> number_of_servos = 1;
-	hls::stream<uint8_t> ics_char;
-	hls::stream<uint1_t> ics_sig_i;
+TEST_F(IcsIfRxTest, ReceiveTest) {
+    bit_period = 87;
+    //initialize ics_sig_i
+    //cyclic0
+    //cyclic0 servo_index0
+    input_idle(ics_sig_i,100);
+    input_stream(ics_sig_i,0x80,bit_period,0);ics_char_exp.write(0x80);//loopback
+    input_stream(ics_sig_i,0x03,bit_period,0);ics_char_exp.write(0x03);//loopback
+    input_stream(ics_sig_i,0x02,bit_period,0);ics_char_exp.write(0x02);//loopback
+    input_idle(ics_sig_i,100);
+    input_stream(ics_sig_i,0x00,bit_period,0);ics_char_exp.write(0x00);//receive
+    input_stream(ics_sig_i,0x04,bit_period,0);ics_char_exp.write(0x04);//receive
+    input_stream(ics_sig_i,0x05,bit_period,0);ics_char_exp.write(0x05);//receive
+    //cyclic0 servo_index1
+    input_stream(ics_sig_i,0x81,bit_period,0);ics_char_exp.write(0x81);//loopback
+    input_stream(ics_sig_i,0x06,bit_period,0);ics_char_exp.write(0x06);//loopback
+    input_stream(ics_sig_i,0x05,bit_period,0);ics_char_exp.write(0x05);//loopback
+    input_idle(ics_sig_i,100);
+    input_stream(ics_sig_i,0x01,bit_period,0);ics_char_exp.write(0x01);//receive
+    input_stream(ics_sig_i,0x14,bit_period,0);ics_char_exp.write(0x14);//receive
+    input_stream(ics_sig_i,0x15,bit_period,0);ics_char_exp.write(0x15);//receive
+    //cyclic0 servo_index2
+    input_stream(ics_sig_i,0x82,bit_period,0);ics_char_exp.write(0x82);//loopback
+    input_stream(ics_sig_i,0x09,bit_period,0);ics_char_exp.write(0x09);//loopback
+    input_stream(ics_sig_i,0x08,bit_period,0);ics_char_exp.write(0x08);//loopback
+    input_idle(ics_sig_i,100);
+    input_stream(ics_sig_i,0x02,bit_period,0);ics_char_exp.write(0x02);//receive
+    input_stream(ics_sig_i,0x24,bit_period,0);ics_char_exp.write(0x24);//receive
+    input_stream(ics_sig_i,0x25,bit_period,0);ics_char_exp.write(0x25);//receive
+    //cyclic0 servo_index31
+    input_stream(ics_sig_i,0x9f,bit_period,0);ics_char_exp.write(0x9f);//loopback
+    input_stream(ics_sig_i,0x0c,bit_period,0);ics_char_exp.write(0x0c);//loopback
+    input_stream(ics_sig_i,0x0b,bit_period,0);ics_char_exp.write(0x0b);//loopback
+    input_idle(ics_sig_i,100);
+    input_stream(ics_sig_i,0x1f,bit_period,0);ics_char_exp.write(0x1f);//receive
+    input_stream(ics_sig_i,0x34,bit_period,0);ics_char_exp.write(0x34);//receive
+    input_stream(ics_sig_i,0x35,bit_period,0);ics_char_exp.write(0x35);//receive
+    //cyclic1
+    //cyclic1 servo_index0
+    input_stream(ics_sig_i,0xa0,bit_period,0);ics_char_exp.write(0xa0);//loopback
+    input_stream(ics_sig_i,0x03,bit_period,0);ics_char_exp.write(0x03);//loopback
+    input_idle(ics_sig_i,100);
+    input_stream(ics_sig_i,0x20,bit_period,0);ics_char_exp.write(0x20);//receive
+    input_stream(ics_sig_i,0x03,bit_period,0);ics_char_exp.write(0x03);//receive
+    input_stream(ics_sig_i,0x44,bit_period,0);ics_char_exp.write(0x44);//receive
+    //cyclic1 servo_index1
+    input_stream(ics_sig_i,0xa1,bit_period,0);ics_char_exp.write(0xa1);//loopback
+    input_stream(ics_sig_i,0x03,bit_period,0);ics_char_exp.write(0x03);//loopback
+    input_idle(ics_sig_i,100);
+    input_stream(ics_sig_i,0x21,bit_period,0);ics_char_exp.write(0x21);//receive
+    input_stream(ics_sig_i,0x03,bit_period,0);ics_char_exp.write(0x03);//receive
+    input_stream(ics_sig_i,0x45,bit_period,0);ics_char_exp.write(0x45);//receive
+    //cyclic1 servo_index2
+    input_stream(ics_sig_i,0xa2,bit_period,0);ics_char_exp.write(0xa2);//loopback
+    input_stream(ics_sig_i,0x03,bit_period,0);ics_char_exp.write(0x03);//loopback
+    input_idle(ics_sig_i,100);
+    input_stream(ics_sig_i,0x22,bit_period,0);ics_char_exp.write(0x22);//receive
+    input_stream(ics_sig_i,0x03,bit_period,0);ics_char_exp.write(0x03);//receive
+    input_stream(ics_sig_i,0x46,bit_period,0);ics_char_exp.write(0x46);//receive
+    //cyclic1 servo_index31
+    input_stream(ics_sig_i,0xbf,bit_period,0);ics_char_exp.write(0xbf);//loopback
+    input_stream(ics_sig_i,0x03,bit_period,0);ics_char_exp.write(0x03);//loopback
+    input_idle(ics_sig_i,100);
+    input_stream(ics_sig_i,0x3f,bit_period,0);ics_char_exp.write(0x3f);//receive
+    input_stream(ics_sig_i,0x03,bit_period,0);ics_char_exp.write(0x03);//receive
+    input_stream(ics_sig_i,0x47,bit_period,0);ics_char_exp.write(0x47);//receive
+    //cyclic2
+    //cyclic2 servo_index0
+    input_stream(ics_sig_i,0xa0,bit_period,0);ics_char_exp.write(0xa0);//loopback
+    input_stream(ics_sig_i,0x04,bit_period,0);ics_char_exp.write(0x04);//loopback
+    input_idle(ics_sig_i,100);
+    input_stream(ics_sig_i,0x20,bit_period,0);ics_char_exp.write(0x20);//receive
+    input_stream(ics_sig_i,0x04,bit_period,0);ics_char_exp.write(0x04);//receive
+    input_stream(ics_sig_i,0x48,bit_period,0);ics_char_exp.write(0x48);//receive
+    //cyclic2 servo_index1
+    input_stream(ics_sig_i,0xa1,bit_period,0);ics_char_exp.write(0xa1);//loopback
+    input_stream(ics_sig_i,0x04,bit_period,0);ics_char_exp.write(0x04);//loopback
+    input_idle(ics_sig_i,100);
+    input_stream(ics_sig_i,0x21,bit_period,0);ics_char_exp.write(0x21);//receive
+    input_stream(ics_sig_i,0x04,bit_period,0);ics_char_exp.write(0x04);//receive
+    input_stream(ics_sig_i,0x49,bit_period,0);ics_char_exp.write(0x49);//receive
+    //cyclic2 servo_index2
+    input_stream(ics_sig_i,0xa2,bit_period,0);ics_char_exp.write(0xa2);//loopback
+    input_stream(ics_sig_i,0x04,bit_period,0);ics_char_exp.write(0x04);//loopback
+    input_idle(ics_sig_i,100);
+    input_stream(ics_sig_i,0x22,bit_period,0);ics_char_exp.write(0x22);//receive
+    input_stream(ics_sig_i,0x04,bit_period,0);ics_char_exp.write(0x04);//receive
+    input_stream(ics_sig_i,0x4a,bit_period,0);ics_char_exp.write(0x4a);//receive
+    //cyclic2 servo_index31
+    input_stream(ics_sig_i,0xbf,bit_period,0);ics_char_exp.write(0xbf);//loopback
+    input_stream(ics_sig_i,0x04,bit_period,0);ics_char_exp.write(0x04);//loopback
+    input_idle(ics_sig_i,100);
+    input_stream(ics_sig_i,0x3f,bit_period,0);ics_char_exp.write(0x3f);//receive
+    input_stream(ics_sig_i,0x04,bit_period,0);ics_char_exp.write(0x04);//receive
 
-
-	//initialize ics_sig_i
-	//cyclic0
-	//cyclic0 servo_index0
-	input_idle(ics_sig_i,100);
-	input_stream(ics_sig_i,0x80,bit_period);//loopback
-	input_stream(ics_sig_i,0x03,bit_period);//loopback
-	input_stream(ics_sig_i,0x02,bit_period);//loopback
-	input_idle(ics_sig_i,100);
-	input_stream(ics_sig_i,0x00,bit_period);//receive
-	input_stream(ics_sig_i,0x04,bit_period);//receive
-	input_stream(ics_sig_i,0x05,bit_period);//receive
-	//cyclic0 servo_index1
-	input_stream(ics_sig_i,0x81,bit_period);//loopback
-	input_stream(ics_sig_i,0x06,bit_period);//loopback
-	input_stream(ics_sig_i,0x05,bit_period);//loopback
-	input_idle(ics_sig_i,100);
-	input_stream(ics_sig_i,0x01,bit_period);//receive
-	input_stream(ics_sig_i,0x14,bit_period);//receive
-	input_stream(ics_sig_i,0x15,bit_period);//receive
-	//cyclic0 servo_index2
-	input_stream(ics_sig_i,0x82,bit_period);//loopback
-	input_stream(ics_sig_i,0x09,bit_period);//loopback
-	input_stream(ics_sig_i,0x08,bit_period);//loopback
-	input_idle(ics_sig_i,100);
-	input_stream(ics_sig_i,0x02,bit_period);//receive
-	input_stream(ics_sig_i,0x24,bit_period);//receive
-	input_stream(ics_sig_i,0x25,bit_period);//receive
-	//cyclic0 servo_index31
-	input_stream(ics_sig_i,0x9f,bit_period);//loopback
-	input_stream(ics_sig_i,0x0c,bit_period);//loopback
-	input_stream(ics_sig_i,0x0b,bit_period);//loopback
-	input_idle(ics_sig_i,100);
-	input_stream(ics_sig_i,0x1f,bit_period);//receive
-	input_stream(ics_sig_i,0x34,bit_period);//receive
-	input_stream(ics_sig_i,0x35,bit_period);//receive
-	//cyclic1
-	//cyclic1 servo_index0
-	input_stream(ics_sig_i,0xa0,bit_period);//loopback
-	input_stream(ics_sig_i,0x03,bit_period);//loopback
-	input_idle(ics_sig_i,100);
-	input_stream(ics_sig_i,0x20,bit_period);//receive
-	input_stream(ics_sig_i,0x03,bit_period);//receive
-	input_stream(ics_sig_i,0x44,bit_period);//receive
-	//cyclic1 servo_index1
-	input_stream(ics_sig_i,0xa1,bit_period);//loopback
-	input_stream(ics_sig_i,0x03,bit_period);//loopback
-	input_idle(ics_sig_i,100);
-	input_stream(ics_sig_i,0x21,bit_period);//receive
-	input_stream(ics_sig_i,0x03,bit_period);//receive
-	input_stream(ics_sig_i,0x45,bit_period);//receive
-	//cyclic1 servo_index2
-	input_stream(ics_sig_i,0xa2,bit_period);//loopback
-	input_stream(ics_sig_i,0x03,bit_period);//loopback
-	input_idle(ics_sig_i,100);
-	input_stream(ics_sig_i,0x22,bit_period);//receive
-	input_stream(ics_sig_i,0x03,bit_period);//receive
-	input_stream(ics_sig_i,0x46,bit_period);//receive
-	//cyclic1 servo_index31
-	input_stream(ics_sig_i,0xbf,bit_period);//loopback
-	input_stream(ics_sig_i,0x03,bit_period);//loopback
-	input_idle(ics_sig_i,100);
-	input_stream(ics_sig_i,0x3f,bit_period);//receive
-	input_stream(ics_sig_i,0x03,bit_period);//receive
-	input_stream(ics_sig_i,0x47,bit_period);//receive
-	//cyclic2
-	//cyclic2 servo_index0
-	input_stream(ics_sig_i,0xa0,bit_period);//loopback
-	input_stream(ics_sig_i,0x04,bit_period);//loopback
-	input_idle(ics_sig_i,100);
-	input_stream(ics_sig_i,0x20,bit_period);//receive
-	input_stream(ics_sig_i,0x04,bit_period);//receive
-	input_stream(ics_sig_i,0x48,bit_period);//receive
-	//cyclic2 servo_index1
-	input_stream(ics_sig_i,0xa1,bit_period);//loopback
-	input_stream(ics_sig_i,0x04,bit_period);//loopback
-	input_idle(ics_sig_i,100);
-	input_stream(ics_sig_i,0x21,bit_period);//receive
-	input_stream(ics_sig_i,0x04,bit_period);//receive
-	input_stream(ics_sig_i,0x49,bit_period);//receive
-	//cyclic2 servo_index2
-	input_stream(ics_sig_i,0xa2,bit_period);//loopback
-	input_stream(ics_sig_i,0x04,bit_period);//loopback
-	input_idle(ics_sig_i,100);
-	input_stream(ics_sig_i,0x22,bit_period);//receive
-	input_stream(ics_sig_i,0x04,bit_period);//receive
-	input_stream(ics_sig_i,0x4a,bit_period);//receive
-	//cyclic2 servo_index31
-	input_stream(ics_sig_i,0xbf,bit_period);//loopback
-	input_stream(ics_sig_i,0x04,bit_period);//loopback
-	input_idle(ics_sig_i,100);
-	input_stream(ics_sig_i,0x3f,bit_period);//receive
-	input_stream(ics_sig_i,0x04,bit_period);//receive
-	input_stream(ics_sig_i,0x4b,bit_period);//receive
-
-	std::cout << "===============receive test====================" << std::endl;
-	for(int i = 0; i < 64; ++i){
-		while(ics_sig_i.read() == 1);//skip until start bit. it is done by external edge detect logic to assert ap_start.
-		ics_if_rx(ics_sig_i,ics_char,bit_period);
-		std::cout << "char:" << std::setw(2) << std::setfill('0') << std::hex << (int)ics_char.read() << std::endl;
-	}
-	return 0;
+    for(int i = 0; i < 63; ++i){
+        ics_if_rx(ics_sig_i,ics_char,bit_period);
+        EXPECT_EQ(ics_char_exp.read(), ics_char.read()) << "char differ at index 0x" << std::hex << i;
+        //std::cout << "char:" << std::setw(2) << std::setfill('0') << std::hex << (int)ics_char.read() << std::endl;
+    }
 }
+
+TEST_F(IcsIfRxTest, ReceiveParityErrTest) {
+    input_idle(ics_sig_i,100);
+    input_stream(ics_sig_i,0x80,bit_period,1);ics_char_exp.write(0x180);//loopback
+    input_stream(ics_sig_i,0x03,bit_period,1);ics_char_exp.write(0x103);//loopback
+    input_stream(ics_sig_i,0x02,bit_period,1);ics_char_exp.write(0x102);//loopback
+    for(int i = 0; i < 3; ++i){
+        ics_if_rx(ics_sig_i,ics_char,bit_period);
+        EXPECT_EQ(ics_char_exp.read(), ics_char.read()) << "char differ at index 0x" << std::hex << i;
+    }
+}
+/*
+int old_main(void){
+    std::cout << "===============receive test====================" << std::endl;
+    for(int i = 0; i < 64; ++i){
+        while(ics_sig_i.read() == 1);//skip until start bit. it is done by external edge detect logic to assert ap_start.
+        ics_if_rx(ics_sig_i,ics_char,bit_period,0);
+        std::cout << "char:" << std::setw(2) << std::setfill('0') << std::hex << (int)ics_char.read() << std::endl;
+    }
+    return 0;
+}*/
